7cf14218f32cf95f7c93729b1117114d242e4e2f
Anish Shakya <64535767+Anish-shakya@users.noreply.github.com>
2023-06-21 16:50:33 +0545

    Add files via upload

---
+++ First_program.R
@@ -0,0 +1,18 @@
+#My First R Program
+
+print("Coding in R")
+?print()
+
+#Example of variable
+##string data type
+first_variable <- "This is my Variable"
+first_variable
+
+#numeric datatype
+second_variable <- 12.5
+
+#vectors in R
+vec_1 <- c(13,48.5,71,101.5,2)
+vec_1
+
+
