cb170dcdf29ed4bd6ba60a348f5684a019037e1a
Anish Shakya <64535767+Anish-shakya@users.noreply.github.com>
2023-06-27 17:02:31 +0545

    Add files via upload

---
+++ Operator.R
@@ -0,0 +1,12 @@
+## our first calculation
+
+quater_1_sales <- 35657.98
+quater_2_sales <- 43810.55
+
+##addition
+midyear_sales <- quater_1_sales + quater_2_sales
+
+print(midyear_sales)
+
+yearend_sales <-  midyear_sales*2
+print(yearend_sales)
