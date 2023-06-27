18730401bfac1c749e592e75b94349c29a424cac
Anish Shakya <64535767+Anish-shakya@users.noreply.github.com>
2023-06-27 17:11:49 +0545

    Add files via upload

---
+++ ConditionalStatement.R
@@ -0,0 +1,32 @@
+
+#if Statement
+
+x <- 4
+
+if(x > 0){
+  
+  print("X is positive number")
+}
+
+#if else Statement
+
+if(x > 0){
+  
+  print("X is positive number")
+} else{
+  print("x is negative number")
+}
+
+
+#else if
+
+y <- 0
+if(y > 0){
+  
+  print("y is positive number")
+} else if (y < 0){
+  print("y is negative number")
+}else{
+  print("Y is netural number")
+}
+
